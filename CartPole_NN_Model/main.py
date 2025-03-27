e885e973789c60685a9b25f442aaccd06f421b27
yuval1847 <117625084+yuval1847@users.noreply.github.com>
2025-03-27 18:31:13 +0200

    Continue the model

--- CartPole_NN_Model/main.py
+++ CartPole_NN_Model/main.py
@@ -1,15 +1,51 @@
+from collections import deque 
+
 import numpy as np
 import random
 import keras
 import gym
 
-# Parameters initialization:
-max_reward = 500
-state_size = 2
-action_size = 4
+class CartPoleModel:
+    """
+    A class which represent the cart pole model of gym
+    """
+    # Parameters initialization:
+    def __init__(self):
+        self.max_reward = 500
+        self.state_size = 2
+        self.action_size = 4
 
-alpha = 0.7
-gamma = 0.2
-epsilon = 0.8
+        self.alpha = 0.1
+        self.gamma = 0.9
+        self.epsilon = 1.0
 
-# The model:
+        self.memory:deque = deque()
+        self.model = None
+        
+        # Creating the enviroment
+        self.env = gym.make('CartPole-v1', render_mode=None)
+
+    # The model:
+    def _build_model(self):
+        # Input: Nothing.
+        # Output: The function creates a NN model.
+        self.model = keras.Sequential()
+
+        # Input & Hidden layers
+        self.model.add(keras.layers.Dense(units=32, input_dim=self.state_size, activation="relu"))
+        self.model.add(keras.layers.Dense(units=32, activation='relu'))
+        
+        # Output layer
+        self.model.add(keras.layers.Dense(units=self.action_size, activation="linear"))
+        
+        # Compilation of the model
+        self.model.compile(loss='mse', optimizer=keras.Adam(learning_rate=self.alpha))
+
+    
+    def _save_model(self):
+        # Input: Nothing.
+        # Output: The function saves the model to a .keras file
+        file_name = "model.keras"
+        self.model.save("fashion_mnist_model.h5")
+
+    
